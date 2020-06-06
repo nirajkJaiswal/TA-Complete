691b8fa953e11fb85f915752aaa2928f5165d34a
niraj kumar <me.nkjaiswal@outlook.com>
2020-06-07 01:54:00 +0530

    Commit: Added Multiples Projects

---
+++ TA-Complete-API-Automation-Framework/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace TA_Complete_API_Automation_Framework
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
